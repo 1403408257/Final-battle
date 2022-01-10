dabbd708804965567f2bbe84a6c532abf26fea98
李福双 <1403408257@qq.com>
2022-01-10 12:47:18 +0800

    Version 1.0

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld :msg="title"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  data() {
+    return {
+      title: '开发者A',
+    };
+  },
+  components: {
+    HelloWorld
+  }
+}
+</script>
